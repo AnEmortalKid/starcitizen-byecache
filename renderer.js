a79adcf07317a0e8bd74957d3947299bebbecd68
Jan Monterrubio <janmonterrubio@gmail.com>
2023-09-28 23:16:13 -0500

    start layout

--- renderer.js
+++ renderer.js
@@ -20,16 +20,36 @@ btn.addEventListener('click', async () => {
 
 // TODO add tag based on filepath (LIVE/PTU/EPTU)
 
+function getEnvFromPath(installPath) {
+  const lastSlash = installPath.lastIndexOf("\\");
+  return installPath.substring(lastSlash + 1);
+}
+
+
 // TODO make better rows
 window.byecache.installs.get().then((res) => {
   const parentContainer = document.getElementById('installs-container');
+
+  const template = document.getElementById('install-template');
   for (var install of res) {
+    const cloned = template.cloneNode(true);
+    cloned.classList.remove('w3-hide')
+
+    const envSpan = cloned.querySelector('[data-kind="environment"]');
+    envSpan.textContent = getEnvFromPath(install.location);
+
+
+    const removeIcon = cloned.querySelector('[data-kind="remove-install"');
+    removeIcon.addEventListener('click', (evt) => console.log(evt));
+    // need to find elements and set the data and clicks n stuff
+
     // probably need to safe format paths for display but keep
     // the data safe
     // avocapture does this iirc
     //const rowData = document.createElement('span');
     //rowData.textContent = install.location;
     console.log(JSON.stringify(install))
+    parentContainer.appendChild(cloned);
     //parentContainer.appendChild(rowData);
   }
 });
