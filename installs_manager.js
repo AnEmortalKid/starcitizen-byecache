94c3b5f40f48359c79f3b9e2ed9e7aa2b58a630b
AnEmortalKid <janmonterrubio@gmail.com>
2023-09-30 13:06:07 -0500

    jsdocs

--- installs_manager.js
+++ installs_manager.js
@@ -20,18 +20,35 @@ function findById(installs, id) {
   return installs.find((elem) => elem.id === id);
 }
 
-// status: success, data: obj (or null) if no changed
-// status: error, data: failure
 
+/**
+ * Install definition.
+ * @typedef {Object} InstallManagerInstall
+ * @property {string} id - identifier for this install
+ * @property {string} location - parent directory of a StarCitizen USER folder
+ * @property {string} backup - directory where files should be backed up to (may be null)
+ */
+
+/**
+ * Result of an install operation.
+ * @typedef {Object} InstallManagerResponse
+ * @property {boolean} success - whether the operation succeeded or not
+ * @property {...InstallManagerInstall} data - created/updated install value for a given install or null if the operation caused no updates
+ * @property {string} error - Error message when an operation did not succeed due to errors
+ */
+
+/**
+ * Adds a new install definition with the give location path
+ * @param {*} folderPath the parent folder where a USER directory exists
+ * @returns {...InstallManagerResponse} {@link InstallManagerResponse} based on the result of this operation
+ */
 function addInstallLocation(folderPath) {
-  // todo add whether updated or not
-
   // require its the parent of the user folder
   const userPath = path.resolve(folderPath, 'USER')
   if (!fs.existsSync(userPath)) {
     return {
       success: false,
-      error: "Directory selected does not contain a USER folder"
+      error: "Directory selected does not contain a USER folder."
     };
   }
 
@@ -56,6 +73,10 @@ function addInstallLocation(folderPath) {
   }
 }
 
+/**
+ * Removes an install based on its identifier
+ * @param {string} id - identifier for the {@link InstallManagerInstall}
+ */
 function removeInstallLocation(id) {
   const installs = appSettings.get(locationsKey, []);
 
@@ -63,44 +84,70 @@ function removeInstallLocation(id) {
   appSettings.set(locationsKey, updated);
 }
 
+/**
+ * @returns the set of all {@link InstallManagerInstall installs}
+ */
 function getInstallLocations() {
   return appSettings.get(locationsKey, []);
 }
 
+/**
+ * Associates the provided directory as the backup location for an install 
+ * @param {string} installId identifier for the {@link InstallManagerInstall install}
+ * @param {string} backupPath path to the backup directory for this install
+ * @returns {...InstallManagerResponse} {@link InstallManagerResponse response} with the result of the operation
+ */
 function setBackup(installId, backupPath) {
-
   const installs = appSettings.get(locationsKey, []);
 
   const found = findById(installs, installId);
   if (!found) {
-    // TODO return some status or a filter or something
-    return false;
+    return { success: false, error: "The provided install could not be found." };
   }
 
   found.backup = backupPath;
   appSettings.set(locationsKey, installs);
+
+  return {
+    success: true,
+    data: found
+  }
 }
 
+/**
+ * Removes the associated backup directory for an install
+ * @param {string} installId identifier for the {@link InstallManagerInstall install}
+ * @returns {...InstallManagerResponse} {@link InstallManagerResponse response} with the result of the operation
+ */
 function removeBackup(installId) {
   const installs = appSettings.get(locationsKey, []);
 
   const found = findById(installs, installId);
   if (!found) {
-    // TODO return some status or a filter or something
-    return false;
+    return { success: false, error: "The provided install could not be found." };
   }
 
   delete found.backup
   appSettings.set(locationsKey, installs);
+
+  return {
+    success: true,
+    data: found
+  }
 }
 
+/**
+ * Removes the USER directory contents for a givne install
+ * 
+ * @param {string} installId - the identifier of {@link InstallManagerInstall install}
+ * @returns {...InstallManagerResponse} {@link InstallManagerResponse response} with the result of the operation
+ */
 function purgeInstall(installId) {
   const installs = appSettings.get(locationsKey, []);
-  const found = findById(installs, installId);
 
+  const found = findById(installs, installId);
   if (!found) {
-    // todo status
-    return;
+    return { success: false, error: "The provided install could not be found." };
   }
 
   // only backup if we specified stuff
@@ -111,6 +158,8 @@ function purgeInstall(installId) {
   // delete the files inside the user dir
   const userDIR = path.resolve(found.location, USER_SUBDIR);
   utils.safeDeleteDirectoryContents(userDIR);
+
+  return { success: true }
 }
 
 function backupFiles(location, backup) {
